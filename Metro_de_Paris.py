8ce18d37bdced6f24f7951d7917cd1c926894831
Thalia Barbosa Marques de Almeida <tbma@ic.ufal.br>
2022-09-26 20:48:16 -0300

    Corrigindo alguns bugs

--- Metro_de_Paris.py
+++ Metro_de_Paris.py
@@ -1,10 +1,5 @@
 
 def receber_st(resposta):
-    '''
-    Recebe uma string com os valores de s e t separadas por espaço e as retorna como duas variáveis diferentes
-    :param resposta: Numeros inseridos pelo usuario
-    :return: s e t separados em variáveis diferentes
-    '''
     numeros = resposta.split()
     inicio = str(numeros[0])
     fim = str(numeros[1])
@@ -13,7 +8,6 @@ def receber_st(resposta):
 def estrela():
     k = 0
     while k > -1:
-        # AINDA NÃO CHEGAMOS...
         for i in range(0, len(fronteira)):
             if visitados[fronteira[i][2]] == 0:
                 break
@@ -36,18 +30,12 @@ def caminho(a):
 def sucessores_de(a):
     for i in range(0, len(estacoes_nomes)):
         if matriz_ligacoes[a[2]][i] != 0 and visitados[a[2]] == 0:
-            #	CALCULAR O G DESSE SUCESSOR
             g = converterParaTempo(a[3][1] + matriz_estacoes[a[2]][i])
-            #	CALCULAR H DESSE SUCESSOR
             h = converterParaTempo(matriz_estacoes[i][target])
-            #	PAI DO SUCESSOR (DE QUE ESTAÇÃO ESSE TREM CHEGOU EM i)
             pai = a
-            #	COR DA LINHA
             linha = matriz_ligacoes[a[2]][i]
             if a[4] != linha:
-                h += tempo_baldeacao	#	ACRESCIMO DO TEMPO GASTO NA TROCA DE LINHA
-            #	ADICIONAR NA FRONTEIRA NOVO SUCESSOR
-            s = [h+g, g, i, pai, linha]
+                h += tempo_baldeacao	
             fronteira.append(s)
     visitados[a[2]] = 1
 
@@ -56,19 +44,13 @@ def converterParaTempo(x):
     return mins
 
 def tempo_final(kilometros_rodados):
-    '''
-    Encontrar o tempo total da viagem do trem, a partir da velocidade media
-    :param kilometros_rodados: kilometragem total da corrida
-    :return: o tempo em minutos
-    '''
+  
     horas = kilometros_rodados/velocidade_media
     minutos = horas * 60
     return minutos
 
-# LISTA REPRESENTANDO A FRONTEIRA USADA PARA A BUSCA DO A*
 fronteira = []
 
-# ARRAY AUXILIAR PARA MARCAÇÃO DE ESTAÇÕES JÁ OU NÃO VISITADAS
 visitados = [0]*14
 
 resposta = []
@@ -76,9 +58,8 @@ resposta = []
 trocasDeEstacao = 0
 km = 0
 
-# Matriz com a distancia em linha reta e real entre as estações
+
 matriz_estacoes = [
-#	 E1     E2    E3    E4      E5   E6     E7    E8    E9    E10   E11   E12  E13   E14
     (0,    10,   18.5, 24.8, 36.4, 38.8,  35.8, 25.4, 17.6, 9.1,  16.7, 27.3, 27.6, 29.8),# E1
     (10,   0,    8.5,  14.8, 26.6, 29.1,  26.1, 17.3, 10,   3.5,  15.3, 20.9, 19.1, 21.8),# E2
     (18.5, 8.5,  0,    6.3,  18.2, 20.6,  17.6, 13.6, 9.4,  10.3, 19.5, 19.1, 18.7, 16.6),# E3
@@ -95,14 +76,9 @@ matriz_estacoes = [
     (29.8, 21.8, 16.6, 15.4, 17.9, 18.2,  15.6, 27.6, 26.6, 21.2, 35.5, 33.6, 5.1,  0)    # E14
     ]
 
-# AZUL     1
-# AMARELA  2
-# VERDE    3
-# VERMELHA 4
 
-# Matriz com as ligações entre as estações(linhas)
+
 matriz_ligacoes = [
-#	E0  E1  E2 E3 E4 E5 E6 E7 E8 E9 E10 E11 E12 E13
     (0, 1,  0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0),# E0
     (1, 0,  1, 0, 0, 0, 0, 0, 2,  2,  0,  0,  0,  0),# E1
     (0, 1,  0, 1, 0, 0, 0, 0, 4,  0,  0,  0,  4,  0),# E2
@@ -119,22 +95,15 @@ matriz_ligacoes = [
     (0, 0,  0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  3,  0) # E13
     ]
 
-# Receber a estação de início e fim
 estacoes = str(input('Insira a estação de início e destino, respectivamente e separadas por espaços (Ex.: 1 3): ')).strip()
 start, target = receber_st(resposta=estacoes)
 start -= 1
 target -= 1
-# Nome das estações
 estacoes_nomes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
-# Recebendo velocidade do trem
 velocidade_media = int(input('Velocidade média do trem em km/h: '))
-# Receber o tempo de baldeação
 tempo_baldeacao = int(input('Tempo para trocar de metrô, em minutos: '))
-# Calculos
 V = [0, 0, -1, -1, -1]
-# lista com nomes das estações visitadas
 nomes_visitados = []
-# E = [f, g, indiceEstacao, pai, corDaLinha]
 a = [converterParaTempo(0 + matriz_estacoes[start][target]), 0, start, V, None]
 
 fronteira.append(a)
@@ -143,7 +112,6 @@ estrela()
 
 final = resposta[::-1]
 
-#printar as estações na ordem que foram expandidas pelo programa
 for pos in range(0, len(nomes_visitados)):
     print(f'{nomes_visitados[pos]}', end='-')
 print(f'{target + 1}')
